c88743fd82402836726eb50afc4fdc4af572c706
Brian Flynn <bdflynny@gmail.com>
2019-02-11 17:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-document.addEventListener("DOMContentloaded", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  
+  let currentMovie = "Titanic"
+  
+  document.addEventListener("click", (e) => {
+    currentMovie = e.target.innerText
+    fillContent(currentMovie)
+  })
+  
+  let title = document.querySelector("#title");
+  let director = document.querySelector("#director")
+  let genre = document.querySelector("#genre");
+  let filmRating = document.querySelector("#filmRating")
+  let poster = document.querySelector("#poster")
+  let description = document.querySelector("#description")
+  let audienceScore = document.querySelector("#audienceScore")
+  let cast = document.querySelector("#cast")
+  
+  const fillContent = function(currentMovie) {
+  title.innerHTML = movies[currentMovie].title
+  
+  director.innerHTML = movies[currentMovie].director
+  
+  genre.innerHTML = movies[currentMovie].genre
+  
+  filmRating.innerHTML = movies[currentMovie].filmRating
+  
+  poster.setAttribute("src", movies[currentMovie].poster)
+  
+  description.innerHTML = movies[currentMovie].description
+  
+  audienceScore.innerHTML = movies[currentMovie].audienceScore
+  
+  console.log(movies[currentMovie].cast[0].actor)
+  for(i=0; i< movies[currentMovie].cast.length; i++){
+    p = document.createElement("p")
+    p.innerText = movies[currentMovie].cast[i].actor
+    cast.appendChild(p)
+  }
+  
+  }
+  
+  
+  fillContent(currentMovie)
+  
 });
+
+
